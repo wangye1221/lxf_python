96c021ee1e56282d1020aa06357151025ab2f1d3
wangye <f_totti_ac@sina.com>
2017-06-12 21:50:23 +0800

    test

--- hello.py
+++ hello.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-#print('Hello,world!')
+print('Hello,world!')
 
 name = input('Please input your name: ')
 print('Hello, ' + name)
+
+a = 100
+if a >= 100:
+    print(a)
+else:
+    a = -a
+
+str = '中文'
+print(str + '的长度是%s' % len(str))
+
+for x in range(5):
+    print(x)
