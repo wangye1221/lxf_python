561d4b9c78b802e8a5b3cc5fe9cce71316cb1352
wangye <f_totti_ac@sina.com>
2017-06-12 22:43:18 +0800

    tkinter

---
+++ tkinter.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from tkinter import *
+
+class Application(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.pack()
+        self.createWidgets()
+    
+    def createWidgets(self):
+        self.helloLabel = Label(self, text='Hello, world!')
+        self.helloLabel.pack()
+        self.quitButton = Button(self, text='退出', command=self.quit)
+        self.quitButton.pack()
+
+app = Application()
+app.master.title('Hello World')
+app.mainloop()
