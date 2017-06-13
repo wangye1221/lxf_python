110013fd6a8003df22ab5d2f61efa0e5d0c81d13
wangye <f_totti_ac@sina.com>
2017-06-13 14:47:00 +0800

    tkinter

--- tkinter.py
+++ tkinter.py
@@ -8,13 +8,15 @@ class Application(Frame):
         Frame.__init__(self, master)
         self.pack()
         self.createWidgets()
-    
+
     def createWidgets(self):
         self.helloLabel = Label(self, text='Hello, world!')
         self.helloLabel.pack()
-        self.quitButton = Button(self, text='退出', command=self.quit)
+        self.quitButton = Button(self, text='Quit', command=self.quit)
         self.quitButton.pack()
 
 app = Application()
+# 设置窗口标题:
 app.master.title('Hello World')
+# 主消息循环:
 app.mainloop()
